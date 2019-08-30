b18e36191f1b300cff626cbb9013c44e0220439f
Your Name <nktclt@gmail.com>
2019-08-30 13:18:23 +0530

    first commit

---
+++ dataset_generator.py
@@ -0,0 +1,20 @@
+from PIL import Image, ImageDraw, ImageFont
+from random import randint
+
+
+lo = 300
+fontsize = 40
+
+
+def random_with_N_digits(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return randint(range_start, range_end)
+
+
+for m in range(lo):
+    font = ImageFont.truetype("arial.ttf", fontsize)
+    img = Image.new('RGB', (320, 320), color=(0+m, 0+m*1, 0+m*2))
+    d = ImageDraw.Draw(img)
+    d.text((10,50), str('+91')+str(random_with_N_digits(10)), font=font, fill=(255, 255, 255))
+    img.save('/home/navaneeth/work/OCR-darknet/images/'+str(m)+'.png')
